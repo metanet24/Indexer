d339aabf4e328f00de68f0d25e5c8492a6dc43bb
unknown <matanatna@code.edu.az>
2023-11-04 12:10:37 -0700

    add

---
+++ İndexerHomeWork/İndexerHomeWork/PersonList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace İndexerHomeWork
+{
+    public class PersonList
+    {
+
+        public Person  GetById(int id)
+        {
+            DataList<Person> person = new();
+
+            Person[] res =person.GetAll();
+
+            Person result=res.FirstOrDefault(x=>x.Id==id);
+
+            return result;
+        }
+
+        public Person Delete(int id)
+        {
+            DataList<Person> person = new();
+
+            
+            Person[] res=person.GetAll();
+
+            Person[] per = new Person[5];
+
+            foreach(Person p in res)
+            {
+                if (p.Id == id)
+                {
+                    continue;
+                }
+                
+                  
+               
+            }
+        }
+   
+
+    }
+}
